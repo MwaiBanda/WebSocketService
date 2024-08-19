67ecb8f09494c701a491fe8b0e1ea444937e9595
Mwai Banda <mwai.banda@cbn.org>
2024-08-19 02:24:47 -0500

    Added port env variable

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
 	"PrayerService/controller"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 
 func main()  {
 	controller := controller.NewController()
-
+	port := func() string {
+		if len(os.Getenv("PORT")) > 0 {
+			return os.Getenv("PORT")
+		} else {
+			return "8080"
+		}
+	}()
 	http.HandleFunc("/subscribe", controller.Subscribe)
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(":" + port, nil); err != nil {
 		fmt.Println("Server error:", err)
 	}
 }
