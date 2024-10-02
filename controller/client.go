5e9440944626f894599ba1135f989000ce30306b
Mwai Banda <mwai.banda@cbn.org>
2024-10-02 14:11:01 -0500

    Unignored dist files

--- controller/client.go
+++ controller/client.go
@@ -1,12 +1,8 @@
 package controller
 
-import "github.com/gorilla/websocket"
-
 type Client struct {
 	id string
 	deviceId string
-	controller *Controller
-	conn *websocket.Conn
 	// Buffered func of outbound messages.
 	send func(int, []byte)
 }
--- controller/subscribe.go
+++ controller/subscribe.go
@@ -27,11 +27,9 @@ func (controller *Controller) Subscribe(w http.ResponseWriter, r *http.Request) 
 	client := &Client{
 		id: clientId,
 		deviceId: deviceId,
-		controller: controller,
-		conn: conn,
 		send: func(messageType int, message []byte) {
 			if err := conn.WriteMessage(messageType, message); err != nil {
-				// controller.RemoveClient(clientId)
+				controller.RemoveClient(clientId)
 				conn.Close()
 				log.Println("client.send", err)
 				return
@@ -54,7 +52,7 @@ func (controller *Controller) Subscribe(w http.ResponseWriter, r *http.Request) 
 			fmt.Println("Message Type:", messageType)
 			fmt.Println(string(message))
 			if err != nil {
-				// controller.RemoveClient(clientId)
+				controller.RemoveClient(clientId)
 				conn.Close()
 				log.Println(err)
 				return
